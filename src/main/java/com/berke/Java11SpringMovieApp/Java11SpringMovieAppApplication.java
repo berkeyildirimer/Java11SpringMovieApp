a5f4225a98a6417ac7924cc79bdbfb67876dbdc4
Berke <r.berkeyildirimer@gmail.com>
2023-10-27 09:27:12 +0300

    Initial commit

---
+++ src/main/java/com/berke/Java11SpringMovieApp/Java11SpringMovieAppApplication.java
@@ -0,0 +1,13 @@
+package com.berke.Java11SpringMovieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11SpringMovieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11SpringMovieAppApplication.class, args);
+	}
+
+}
